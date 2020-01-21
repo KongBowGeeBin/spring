7b8f7f2bdcda55e479ba38ff824907adda4fce57
KongBowGeeBin <hjh163-nba@163.com>
2020-01-21 17:55:27 +0800

    lession03

---
+++ lession03/src/main/java/com/hjh/lession03/JSONToString.java
@@ -0,0 +1,4 @@
+package com.hjh.lession03;
+
+public class JSONToString {
+}
---
+++ lession03/src/main/java/com/hjh/lession03/Lession03Application.java
@@ -0,0 +1,13 @@
+package com.hjh.lession03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lession03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lession03Application.class, args);
+    }
+
+}
